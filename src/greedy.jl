5921ad7658084662270b9ad8c0de03cb761f63d5
Arthur <arthur.divanovic@polytechnique.edu>
2023-10-17 17:32:35 +0200

    Greedy with proportions

--- src/greedy.jl
+++ src/greedy.jl
@@ -1,3 +1,17 @@
+"""
+    greedy_proportion(adj::Matrix{Int}, k::Int, colors::Vector{Int})::Vector{Int}
+
+Greedy heuristic that iterates over the vertices and chooses the first available color. 
+If no color is available, a random one is picked.
+
+# Arguments 
+- adj::Matrix{Int} : adjacendy matrix of the graph
+- k::Int : number of colors
+- colors::Vector{Int} : color table that associates a vertice number to it's color before the execution
+
+# Outputs
+- colors::Vector{Int} : updated color table
+"""
 function greedy_coloring(adj::Matrix{Int}, k::Int, colors::Vector{Int})::Vector{Int}
     n = size(adj)[1]
 
@@ -15,6 +29,55 @@ function greedy_coloring(adj::Matrix{Int}, k::Int, colors::Vector{Int})::Vector{
         else
             colors[u] = new_c
         end
+    end
+    
+    return colors
+end
+
+"""
+    greedy_proportion(adj::Matrix{Int}, k::Int, colors::Vector{Int})::Vector{Int}
+
+Greedy heuristic that iterates over the vertices and chooses the available color that is used by a minimimum number of vertices. 
+If no color is available, a random one is picked.
+
+# Arguments 
+- adj::Matrix{Int} : adjacendy matrix of the graph
+- k::int : number of colors
+- colors::Vector{Int} : color table that associates a vertice number to it's color before the execution
+
+# Outputs
+- colors::Vector{Int} : updated color table
+"""
+function greedy_proportion(adj::Matrix{Int}, k::Int, colors::Vector{Int})::Vector{Int}
+    n = size(adj)[1]
+    indices = collect(1:k)
+    
+    props = zeros(k)
+    for u = 1:n
+        props[colors[u]] += 1
+    end
+
+    for u = 1:n
+        old_c = colors[u]
+
+        c_available = trues(k)
+        for v = 1:n
+            if adj[u,v] == 1
+                c_available[colors[v]] = false
+            end
+        end
+        
+        new_c = 1
+        if any(c_available)
+            idx = argmin(props[c_available])
+            new_c = indices[c_available][idx]
+        else
+            new_c = rand(1:k)
+        end
+
+        colors[u] = new_c
+        props[new_c] += 1
+        props[old_c] -= 1
     end
     
     return colors
