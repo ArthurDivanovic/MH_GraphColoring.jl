465e7114ebd63acf4a9861d2e313ac3b52eb2090
navaxel <axel.navarro@polytechnique.edu>
2023-10-19 11:16:09 +0200

    modif colored graph +n +m

--- src/coloredgraph.jl
+++ src/coloredgraph.jl
@@ -2,15 +2,18 @@ include("parser.jl")
 
 struct ColoredGraph
     adj     ::Matrix{Int}
+    n       ::Int
+    m       ::Int
     k       ::Int
     colors  ::Vector{Int}
 end
 
 function init_graph(graph_path::String, k_path::String, k_idx::Int=1)::ColoredGraph
     adj, m, file_name = parse_file(graph_path)
+    n = size(adj)[1]
     k_dict = k_parser(k_path)
     k = k_dict[file_name][k_idx]
-    colors = ones(Int, size(adj)[1])
+    colors = ones(Int, )
 
-    return ColoredGraph(adj, k, colors)
+    return ColoredGraph(adj, n, m, k, colors)
 end
